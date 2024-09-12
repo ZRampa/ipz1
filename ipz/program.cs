69fd0be728d0b852c0d61a16463e4a84068da899
Rampa <zhenialazorenkonetbook@knu.ua>
2024-09-12 22:44:47 +0300

    Add project files.

---
+++ ipz/program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipz
+{
+    public class program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n; 
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть номер числа Фібоначчі: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = Fibonacci(n);
+
+            Console.WriteLine($"Число Фібоначчі для n = {n} дорівнює {result}");
+        }
+    }
+}
